e3ce6d836553560e9e39df9b4a6df958085c0e5c
yasharyas <yasharyas@proton.me>
2024-05-15 17:56:58 +0530

    created get cameraResponse functionality

--- src/main/java/com/sony/camera/response/CameraResponse.java
+++ src/main/java/com/sony/camera/response/CameraResponse.java
@@ -1,4 +1,15 @@
 package com.sony.camera.response;
 
+import lombok.Data;
+import java.util.List;
+
+@Data
 public class CameraResponse {
+    private String id;
+    private String model;
+    private String resolution;
+    private String sensorType;
+    private double price;
+    private List<String> features;
+    private String status;
 }
